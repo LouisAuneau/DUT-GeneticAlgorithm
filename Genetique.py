7d70aee76a590d28511476ffd072d033360a29d7
Louis Auneau <louis.auneau.c@gmail.com>
2015-11-28 11:00:07 +0000

    Ajout de la mutation, et du graphique de statistiques, correctifs

--- Genetique.py
+++ Genetique.py
@@ -1,8 +1,12 @@
+# coding: utf8
 from math import ceil
 from math import factorial
 from math import floor
 from random import sample
+from random import randint
+from random import shuffle
 from Chemin import Chemin
+
 
 class Genetique:
     def __init__(self, carte):
@@ -44,14 +48,14 @@ class Genetique:
         return distance
     
     def crossovers(self, population):
-        for i in range(0, len(population), 2):
+        for i in range(2, len(population), 2):
             self.crossover(population[i], population[i+1])
             
                 
     def crossover(self, chemin1, chemin2):
         # On genere les deux points de coupe
         nbVilles = len(chemin1.chemin)
-        pointsCrossover = sample(range(0, nbVilles), 2)
+        pointsCrossover = sample(range(0, nbVilles), 2) # Sample genere des nombres aleatoires sans repetition. Param 1 : AMplitude. Param 2 : Nombre de chiffres
         pointsCrossover.sort()
         
         # On echange les villes des deux chemins entre les deux points de coupe et on enleve si il y a un doublon
@@ -78,14 +82,48 @@ class Genetique:
                 chemin2.chemin[i] = villesManquantesChemin2[0]
                 villesManquantesChemin2.pop(0)
     
-    def generationSuivante(self, population):
+    def generationSuivante(self, population, carte):
         moitier = int(ceil((len(population)/2) / 2.) * 2)
         population = sorted(population, key=lambda chemin: chemin.evaluation)
+        
+        # Affichage de test
+        '''
+        print "----------\nETAPE\n------------"
+        for chemin in population:
+            print str(chemin.chemin) + "=>" + str(chemin.calculDistanceReelle(carte))
+        '''
+        
         nouvellePopulation = []
         for i in range(0, moitier):
             nouvellePopulation.append(population[i])
+        copy = nouvellePopulation[1:]
+        shuffle(copy)
+        nouvellePopulation[1:]
         return nouvellePopulation
         
         
-        
+    
+    def mutation(self, population, carte):
+        """
+            Réalise la mutation de la population.
+            
+            :param population: Tableau de chemin représentant la population à faire muter
+            :type population: list
+        """
+        rand = randint(0, 10)
+        for chemin in population:
+            if(rand < 2 and len(chemin.chemin) > 4):
+                mutations = sample(range(0, len(chemin.chemin)-1), 2)
+                pt1 = chemin.chemin[mutations[0]]
+                pt2 = chemin.chemin[mutations[0] + 1]
+                pt3 = chemin.chemin[mutations[1]]
+                pt4 = chemin.chemin[mutations[1] + 1]
+                
+                distance12_34 = carte.getDistance(carte.villes[pt1], carte.villes[pt2]) + carte.getDistance(carte.villes[pt3], carte.villes[pt4])
+                distance13_24 = carte.getDistance(carte.villes[pt1], carte.villes[pt3]) + carte.getDistance(carte.villes[pt2], carte.villes[pt4])
+                
+                if distance13_24 < distance12_34:
+                    chemin.chemin[pt2], chemin.chemin[pt3] = chemin.chemin[pt3], chemin.chemin[pt2]
+                    
+            
         
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+# coding: utf8
 from Carte import Carte
 from Ville import Ville
 from Chemin import Chemin
 from Genetique import Genetique
 from random import randint
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+# Variables
+results = []
+carte = Carte()
 
 # Creation d'une carte
-carte = Carte()
 ville1 = Ville(0, 0)
 ville2 = Ville(100, 0)
 ville3 = Ville(0, 100)
@@ -14,6 +22,7 @@ ville5 = Ville(70, 75)
 ville6 = Ville(25, 40)
 ville7 = Ville(40, 80)
 ville8 = Ville(90, 15)
+ville9 = Ville(25, 80)
 carte.ajoutVille(ville1)
 carte.ajoutVille(ville2)
 carte.ajoutVille(ville3)
@@ -22,33 +31,59 @@ carte.ajoutVille(ville5)
 carte.ajoutVille(ville6)
 carte.ajoutVille(ville7)
 carte.ajoutVille(ville8)
+carte.ajoutVille(ville9)
 
 # Generation de la population initiale scahant que l'on a n! chemins possibles.
 algo = Genetique(carte)
 population = algo.getpopulationInitiale()
 i = 1
 
+# Boucle de générations
 while True:
-    # Crossover de la population initiale
+    # Crossover de la population
     algo.crossovers(population)
+    
+    # Mutation de la population
+    algo.mutation(population, carte)
     
     # Evaluation de la population
     algo.evaluation(population)
     
     # Passage a la generation suivante
-    population = algo.generationSuivante(population)
+    population = algo.generationSuivante(population, carte)
 
-    # Affichage du resultat
+    # Enregistrement des resultats dans un graphe
     couleur = "#%06x" % randint(0, 0xFFFFFF)
-    Chemin.tracerChemins(carte, population, "graphs/etape"+str(i)+".png", couleur)
+    Chemin.tracerChemins(carte, population, "results/etape"+str(i)+".png", couleur)
     
+    # Enregistrement des statistiques de la génération pour le graphe final (Moyenne de la longueur réelle des chemins dans la population)
+    j= 0
+    result = 0
+    print "\nETAPE"
+    for chemin in population:
+        print chemin.calculDistanceReelle(carte)
+        result = result + chemin.calculDistanceReelle(carte)
+        j = j + 1
+    results.append(result/j)
+
+    # Passage à l'étape suivante
     i = i+1
-    
-    if i > 8:
+    if i > 7:
         break
 
-for chemin in population:
-    print str(chemin.chemin) + " -> " + str(chemin.evaluation)
-population[0].tracer(carte, "graphs/final.png", "b")
-print population[0].calculDistanceReelle(carte)
 
+
+# Résultats finales
+# -------------------------
+
+# Tri de la population finale et choix du meilleur resultat
+population[0].tracer(carte, "results/final.png", "black")
+population = sorted(population, key=lambda chemin: chemin.evaluation)
+results.append(population[0].calculDistanceReelle(carte))
+print results
+
+# Création du graphe de statistiques
+fig = plt.figure() # On creer la figure
+ax = fig.add_subplot(111)
+ax.plot(range(1, len(results)+1), results, linestyle='-', marker='o', color='black') # On ajoute la moyenne des longueurs des chemins à chaque étape
+fig.savefig("results/stats.png")
